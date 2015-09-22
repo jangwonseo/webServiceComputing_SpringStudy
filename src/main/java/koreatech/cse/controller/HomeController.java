7a9a836328641cdcba52e8af31c597a9ec132e32
jangwonseo <mklmkl2001@gmail.com>
2015-09-22 18:22:08 +0900

    test

---
+++ src/main/java/koreatech/cse/controller/HomeController.java
@@ -0,0 +1,28 @@
+package koreatech.cse.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @Value("${env.text}")
+    private String env;
+
+    @RequestMapping
+    public String home(Model model) {
+        model.addAttribute("textFromController", "World");
+        return "hello";
+    }
+
+    @RequestMapping("/env")
+    public String env(Model model) {
+        model.addAttribute("textFromController", env);
+        return "hello";
+    }
+}
+
